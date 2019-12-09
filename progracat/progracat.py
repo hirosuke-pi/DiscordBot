9f18e88c51b27ecfc8c122f5abd1c7276065f602
betacode_ <betacode.net@gmail.com>
2019-12-09 23:16:02 +0900

    v1.4 updated
    add minesweeper

--- progracat/progracat.py
+++ progracat/progracat.py
@@ -22,7 +22,7 @@ class ProgramInfo:
         self.fileext = ''  # プログラムファイル拡張子
         self.mention_flag = True # リプライするかどうか
         self.session_id = random.randint(0, 9999) # セッションID
-        self.version = '1.3'
+        self.version = '1.4'
         self.lang = ''
     
     def get_filepath(self):
@@ -255,6 +255,45 @@ async def on_message(message):
                     timeout = int(tmp[1])
                     await message.channel.send(message.author.mention + ' 制限時間を、'+ str(old_time) +'秒から'+ str(timeout)+'秒に変更したぞ！')
                     log('[*] Timeout changed - '+ str(message.author) +': '+ str(old_time) +'s => ' + str(timeout) +'s', info.session_id)
+    
+    elif message.content == '/mine':
+        bomb_list = []
+        num_dict = { 0 : '0⃣', 1 : '1⃣', 2 : '2⃣', 3 : '3⃣', 4 : '4⃣', 5 : '5⃣', 6 : '6⃣', 7 : '7⃣', 8 : '8⃣', 9 : '9⃣'}
+        search_list = ((-1, -1), (0, -1), (1, -1),
+                       (-1, 0),           (1, 0),  
+                       (-1, 1),  (0, 1),  (1, 1))
+        X = 14
+        Y = 14
+
+        # ボム生成
+        for y in range(Y):
+            bomb_list.append([9 if random.randint(0, 15) == 1 else 0 for i in range(X)])
+        
+        # ボム位置の把握
+        for y in range(Y):
+            for x in range(X):
+                count = 0
+                if bomb_list[y][x] != 9:
+                    for s_ptr in search_list:
+                        tmp_x = x + s_ptr[0]
+                        tmp_y = y + s_ptr[1]
+                        if 0 <= tmp_x < X and 0 <= tmp_y < Y:
+                            if bomb_list[tmp_y][tmp_x] == 9:
+                                count += 1
+                    bomb_list[y][x] = count
+
+        # 文字列に変換
+        mine_data = ''
+        for bomb_ptr in bomb_list:
+            #print(bomb_ptr)
+            for bomb in bomb_ptr:
+                if bomb == 9:
+                    mine_data += '||#⃣||'
+                else:
+                    mine_data += '||'+ num_dict[bomb] + '||'
+            mine_data += '\r\n'
+        await message.channel.send(mine_data)
+                
     
     elif message.content == '/comp':
         try:
